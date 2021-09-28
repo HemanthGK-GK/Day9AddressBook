4b272bef1687c90142d0cdd277f332e7685f203d
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-28 15:17:06 +0530

    Hemanth : Delete the Contact in the Address Book

---
+++ UC4DeleteContact/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC4DeleteContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details ob = new Details();
+            ob.choice();
+        }
+    }
+}
